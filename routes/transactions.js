9bd2f2559af51f084b3f277215246a88d0cf5677
CrackedJar <oppewala@gmail.com>
2017-08-21 14:29:18 +1000

    Find a transaction by id

--- routes/transactions.js
+++ routes/transactions.js
@@ -3,6 +3,11 @@ var router = express.Router();
 var moment = require('moment');
 var admin = require('firebase-admin');
 
+var currentID = 3;
+var getNextID = function() {
+    this.currentID += 1;
+    return this.currentID;
+}
 var transactions = [{
         transactionID: 1,
         date: moment("2017-01-01", "YYYY-MM-DD"),
@@ -21,13 +26,23 @@ var transactions = [{
         description: "Transact no 3",
     }];
 
-router.get('/', function(req, res, next) {
+router.all('/', function(req, res, next) {
+    console.log('Request received for transaction router.');
+    next();
+});
+router.get('/', function(req, res) {
   res.json(transactions);
 });
-router.post('/', function(req, res, next) {
+router.get('/:id', function(req, res) {
+    var transaction = transactions.find(function(t) {
+        t.transactionID === req.params.id;
+    });
+    res.json(transaction);
+});
+router.post('/', function(req, res) {
   var transaction = req.body;
 
-  transaction.transactionID = 52; 
+  transaction.transactionID = this.getNextID(); 
 
   transactions.push(transaction);
   res.json(transactions);
